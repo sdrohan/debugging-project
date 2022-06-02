7e9ffea799bf2935a34c07465d04ba27fbb1e0b7
Siobhan Drohan <sdrohan@wit.ie>
2022-06-02 09:43:07 +0100

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    val list = intArrayOf(2, 5, 3, 4)
+    val largestNumber = findLargest(list)
+    println("Largest number is: $largestNumber")
+}
+
+fun findLargest(list: IntArray): Int {
+    var index = 0
+    var max = Int.MAX_VALUE
+
+    while (index < list.size) {
+        if (list[index] > max) {
+            max = list[index]
+        }
+        index++
+    }
+    return max
+}
